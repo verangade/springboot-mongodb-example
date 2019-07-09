6f56fd29718333d308a3f5e44b958d84f12f486e
Malliyawadu Raveera Veranga De Silva  (NCS) <rv.malliyawadu@ncs.com.sg>
2019-07-09 17:40:38 +0800

    Spring boot with mongodb initialization

---
+++ springboot-mongodb-example/springboot-mongodb-example/src/main/java/com/raveera/mongodb/springbootmongodbexample/SpringbootMongodbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.raveera.mongodb.springbootmongodbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbExampleApplication.class, args);
+	}
+
+}
