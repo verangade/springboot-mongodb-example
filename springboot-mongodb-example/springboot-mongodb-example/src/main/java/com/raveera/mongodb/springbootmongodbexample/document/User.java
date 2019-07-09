7b6c65c56d1c3e1cbd8339a613832ceb0a9e7ce0
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-07-09 23:53:15 +0800

    add complete Document - User

--- springboot-mongodb-example/springboot-mongodb-example/src/main/java/com/raveera/mongodb/springbootmongodbexample/document/User.java
+++ springboot-mongodb-example/springboot-mongodb-example/src/main/java/com/raveera/mongodb/springbootmongodbexample/document/User.java
@@ -1,5 +1,97 @@
 package com.raveera.mongodb.springbootmongodbexample.document;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class User {
+	
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private Long salary;
+	private Integer age;
+	
+	public User(Integer id, String firstName, String lastName, Long salary, Integer age) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+		this.age = age;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the salary
+	 */
+	public Long getSalary() {
+		return salary;
+	}
+
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public Integer getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+	
+	
 
 }
