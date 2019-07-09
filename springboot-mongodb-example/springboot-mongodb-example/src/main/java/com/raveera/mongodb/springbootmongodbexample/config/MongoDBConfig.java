1294cae45b59406c2e4207218fc9193ddd147ce5
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-07-10 00:03:07 +0800

    add CommandLineRunner  This CommandLineRunner will be invoked by spring when app satrts up and inject userrepository and will insert records in to mongodb database

--- springboot-mongodb-example/springboot-mongodb-example/src/main/java/com/raveera/mongodb/springbootmongodbexample/config/MongoDBConfig.java
+++ springboot-mongodb-example/springboot-mongodb-example/src/main/java/com/raveera/mongodb/springbootmongodbexample/config/MongoDBConfig.java
@@ -1,13 +1,35 @@
 package com.raveera.mongodb.springbootmongodbexample.config;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import com.raveera.mongodb.springbootmongodbexample.document.User;
 import com.raveera.mongodb.springbootmongodbexample.repository.UserRepository;
 
 @EnableMongoRepositories(basePackageClasses=UserRepository.class)
 @Configuration
 public class MongoDBConfig {
+	
+	/**
+	 * 
+	 * @param userRepository
+	 * @return
+	 * This CommandLineRunner will be invoked by spring when app satrts up and
+	 *  inject userrepository.
+	 * 
+	 */
+	@Bean
+	CommandLineRunner commandLineRunner(UserRepository userRepository) {
+		return new CommandLineRunner() {			
+			@Override
+			public void run(String... args) throws Exception {
+				userRepository.save(new User(1, "Saman", "Kumara", 45000L, 25));
+				userRepository.save(new User(2, "Kamal", "Perera", 35000L, 27));
+			}
+		};
+	}
 	
 
 }
